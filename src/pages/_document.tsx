42222836443022485b0b36637c09bb9134bbc11b
Sacredzero <stormxdevil@gmail.com>
2023-05-13 12:23:42 +0200

    finish

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
-      <body className=' bg-black font-body text-white font-semibold gradient-body'>
+      <Head />
+      <body className="font-body text-white font-semibold gradient-body">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
